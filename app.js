471b08835bfc6d2c63980ae4df16908b04ce6179
Jauri Esteban Cortes Cardenas <jauricardenas18@gmail.com>
2022-03-16 14:52:40 -0500

    cookies

--- app.js
+++ app.js
@@ -30,37 +30,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890'));
 
 function auth (req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
+  
+  if(!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+      
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
 
-  var authHeader = req.headers.authorization;
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
+    var username = auth[0];
+    var password = auth[1];
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', {signed : true})
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
 
-  var username = auth[0];
-  var password = auth[1];
-
-  if (username === 'admin' && password === 'password') {
-    next();
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
   else {
-    var err = new Error('You are not authenticated!');
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
+
 
 app.use(auth);
 
